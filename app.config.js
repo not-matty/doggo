5771cf7ffcf522b7c9586ec7a47f6b53b0ff29c5
not-matty <matthew.deng@warriorlife.net>
2025-02-19 15:07:07 -0800

    hello

--- app.config.js
+++ app.config.js
@@ -8,12 +8,6 @@ export default {
     slug: "doggo",
     version: "1.0.0",
     orientation: "portrait",
-    icon: "./assets/icon.png",
-    splash: {
-      image: "./assets/splash.png",
-      resizeMode: "contain",
-      backgroundColor: "#ffffff"
-    },
     assetBundlePatterns: [
       "**/*"
     ],
@@ -22,14 +16,7 @@ export default {
       bundleIdentifier: "com.notmatty.doggo"
     },
     android: {
-      adaptiveIcon: {
-        foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#FFFFFF"
-      },
       package: "com.notmatty.doggo"
-    },
-    web: {
-      favicon: "./assets/favicon.png"
     },
     extra: {
       eas: {
